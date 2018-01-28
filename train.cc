01c32f853556fa7bc455e0518ad9e8e6a2786841
Royce Fan <fchunhuei@gmail.com>
2018-01-28 21:37:05 +1000

    add argparser.

--- train.cc
+++ train.cc
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <stdexcept>
 #include <caffe2/core/init.h>
 #include <caffe2/core/blob.h>
 #include <caffe2/core/workspace.h>
+#include <gflags/gflags.h>
 #include "mldata.h"
 using namespace std;
 
+DEFINE_string(dataset_path, "", "file path of dataset.");
+DEFINE_string(dataset_label_path, "", "file path of dataset label.");
+
+void parse_arg(int*, char **argv[]);
+
 int main(int argc, char *argv[])
 {
-    cout << argv[1] << endl;
-    cout << argv[2] << endl;
+    parse_arg(&argc, &argv);
+
+    
     auto&& [X, Y] = load_data(argv[1], argv[2]);
 
     for (const auto& x_l : X) {
@@ -35,3 +43,18 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
+void parse_arg(int* argcp, char **argvp[])
+{
+    gflags::ParseCommandLineFlags(argcp, argvp, true);
+    if (!ifstream(FLAGS_dataset_path)) {
+        cerr << "path of dataset not provided." << endl;
+        exit(1);
+    }
+    if (!ifstream(FLAGS_dataset_label_path)) {
+        cerr << "path of dataset label not provided." << endl;
+        exit(1);
+    }
+
+}
+
